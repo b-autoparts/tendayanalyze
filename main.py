7cbf72f783ab5153d8c58cf3bd7ae8739ccea62d
Bhon Bunnag <bhonris@gmail.com>
2021-03-03 10:56:22 +0700

    Add forecast

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import csv
 
 EOF = '9999999999999999'
 
+def sorter(a):
+  return a[0]
+
 def getHeader(line, dates):
   meta, _ = line.split()
   meta = meta.split('|')
   plant = meta[2]
   extras = ["Firm", "1 TEN", "2 TEN", "3 TEN",	"4 TEN", "5 TEN", "2M",	"3M",	"4M"]
-  return ["PLANT " + plant, ''] + dates + extras 
+  return ["Part NO", "PLANT " + plant, ''] + dates + extras 
+
+def getSumheader(header):
+  extras = ["Firm", "1 TEN", "2 TEN", "3 TEN",	"4 TEN", "5 TEN", "2M",	"3M",	"4M"]
+  return ["No", "Part NO", "Plant", "Add"] + list(filter( lambda x: type(x) == int, header)) + extras
 
 
-
-
-def processLine(line):
+def processLine(line, mainDates=None):
   meta, data = line.split()
   meta = meta.split('|')
   data = data.split('|')[1:-1]
@@ -50,64 +55,80 @@ def processLine(line):
       counter_1 += 1
     else:
       if idx > (len(data)-10): otherInfo.append(datum)
-  preInfo = [item for sublist in zip(preDates, preValues) for item in sublist]
+  # preInfo = [item for sublist in zip(preDates, preValues) for item in sublist]
   # info = [item for sublist in zip(dates, values) for item in sublist]
+  dataDict = {}
+  for a,b in zip(dates,values):
+    dataDict[a] = b
+  constructedValues = []
+  if mainDates:
+    for i in mainDates:
+      if i in dataDict:
+        constructedValues.append(dataDict[i])
+      else:
+        constructedValues.append(0)
+    values = constructedValues
   l = ''
   for x,y in zip(preDates, preValues): l = l + str(x) + "/" + str(y) + ", "
   returnInfo = [part] + [plant] + [l] + values + otherInfo
+  # if returnInfo[1] != 'B':
+  #   print(mainDates)
+  #   print(dates)
   return returnInfo, dates
 
-def processFile(f, name):
+def processFile(f, name, header = None, mainDates = None):
   line = f.readline()
   main_data = []
-  data, dates = processLine(line)
-  header = getHeader(line, dates)
+  data, dates = processLine(line, mainDates)
+  if not header:
+    header = getHeader(line, dates)
   if(name[0] == 'D'): # this assumes that everytime we run into the D/F factory case, the file starts with a D
     f_data = []
     headerF = header[:]
-    headerF[0] = 'PLANT F'
+    headerF[1] = 'PLANT F'
     f_data.append(headerF)
-
     d_data = []
     headerD = header[:]
-    headerD[0] = 'PLANT D'
+    headerD[1] = 'PLANT D'
     d_data.append(headerD)
-
-    f_data.append(data) if header[0] == 'PLANT F' else d_data.append(data)
+    f_data.append(data) if header[1] == 'PLANT F' else d_data.append(data)
     while line:
       line = f.readline()
       if EOF in line: break
-      data, _ = processLine(line)
-      header = getHeader(line, dates)
-      if header[0] == 'PLANT F':
+      data, _ = processLine(line, mainDates)
+      altHeader = getHeader(line, dates)
+      if altHeader[1] == 'PLANT F':
         f_data.append(data)
-      if header[0] == 'PLANT D':
+      if altHeader[1] == 'PLANT D':
         d_data.append(data)
     main_data = [headerD] + sumFile(d_data) + [headerF] + sumFile(f_data)
-
   else:
     main_data.append(data)
     while line:
       line = f.readline()
       if EOF in line: break
-      data, _ = processLine(line)
+      data, _ = processLine(line, mainDates)
       main_data.append(data)
     main_data = [header] + sumFile(main_data)
-  return main_data
+  if not mainDates : mainDates = dates
+  return main_data, header, mainDates
 
 def sumFile(DATA):
   PART_DICT = {}
   for datum in DATA:
     partName = datum[0]
-    if 'PLANT ' in partName: continue
+    if 'Part NO' == partName or 'PLANT ' in partName: continue
     if partName in PART_DICT:
       oldData = PART_DICT[partName]
+      if(len(oldData) > len(datum)):
+        datum += [0]*(len(oldData) - len(datum))
+      if(len(oldData) < len(datum)):
+        oldData += [0]*(len(datum) - len(oldData))
       newData = [partName] + [x + y for x, y in zip(datum[1:], oldData[1:])]
       newData[1] = ''.join(list(set(list(newData[1]))))
       PART_DICT[datum[0]] = newData
     else:
-      PART_DICT[datum[0]] = datum
-  
+      PART_DICT[datum[0]] = datum  
   return list(PART_DICT.values()) 
 
 def filterPart(part, plant):
@@ -123,20 +144,30 @@ files = filter(lambda x: not x.endswith('.py'), os.listdir('./'))
 # for name in files:
 # assert(name in FILE_NAMES)
 BIG_DATA = []
+header = None
+mainDates = None
 for NAME in FILE_NAMES:
   with open(NAME) as f:
-    BIG_DATA +=  processFile(f, NAME)
+    temp_data, header, mainDates = processFile(f, NAME, header, mainDates)
+    BIG_DATA +=  temp_data
 
 with open('DATA.csv', 'w', newline='') as f:
   writer = csv.writer(f)
   writer.writerows(BIG_DATA)
-
-SUM_DATA = sumFile(BIG_DATA) 
+SUM_DATA = sumFile(BIG_DATA)
+SUM_DATA.sort(key=sorter) 
+NEW_SUM_DATA = [getSumheader(BIG_DATA[0])] + [[a+1] + b for a,b, in enumerate(SUM_DATA)]
 
 with open('SUM-TENDAY.csv', 'w', newline='') as g:
   writer = csv.writer(g)
-  writer.writerows(SUM_DATA)
+  writer.writerows(NEW_SUM_DATA)
 
+FORCAST_DATA = [[a[0]] + [sum(a[-9:-6])] + [sum(a[-6:-3])] + a[-3:] for a in SUM_DATA]
+
+
+with open('FORCAST.csv', 'w', newline='') as h:
+  writer = csv.writer(h)
+  writer.writerows(FORCAST_DATA)
     
 
 
